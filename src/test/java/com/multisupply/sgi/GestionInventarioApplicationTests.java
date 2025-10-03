6951c22749cbf8f3ac126de2929d0add58a357ed
CodeJomar <codejomar@gmail.com>
2025-10-02 20:07:52 -0500

    inicializando repositorio

---
+++ src/test/java/com/multisupply/sgi/GestionInventarioApplicationTests.java
@@ -0,0 +1,13 @@
+package com.multisupply.sgi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GestionInventarioApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
